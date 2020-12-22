51526f67565510a7d33a40a9b9ec772b83a67735
khuzaima <khuzaimayaseen@gmail.com>
2020-12-23 02:15:17 +0500

    Change package.json

--- next.config.js
+++ next.config.js
@@ -1,32 +1,31 @@
-const { CacheObjectManager } = require('./src/lib/cacheObjectManager');
-const { SitemapManager } = require('./src/lib/sitemapManager');
-const withTM = require('next-transpile-modules')(['lodash-es', 'react-d3-speedometer']);
+const { CacheObjectManager } = require("./src/lib/cacheObjectManager");
+const { SitemapManager } = require("./src/lib/sitemapManager");
+const withTM = require("next-transpile-modules")([
+  "lodash-es",
+  "react-d3-speedometer",
+]);
 
-
-// console.log("Generating sitemap...");
-// SitemapManager.buildSitemap()
-// .then(()=>{
-//     console.log("Sitemap generated.");
-// })
-// .catch((e)=>{
-//     console.log("Sitemap error: ",{e});
-// });
-
+console.log("Generating sitemap...");
+SitemapManager.buildSitemap()
+  .then(() => {
+    console.log("Sitemap generated.");
+  })
+  .catch((e) => {
+    console.log("Sitemap error: ", { e });
+  });
 
 CacheObjectManager.startCacheDaemon();
 
 ///*
 CacheObjectManager.loadWPData()
-.then((storeData)=>{
+  .then((storeData) => {
     //console.log(`Loaded ${storedAmmount} WP posts`);
-    console.log({storeData});
-})
-.catch((err)=>{
-    console.log("Error loading metadata.",{err});
-})
+    console.log({ storeData });
+  })
+  .catch((err) => {
+    console.log("Error loading metadata.", { err });
+  });
 //*/
 
-module.exports = withTM({
-
-})
+module.exports = withTM({});
 //*/
